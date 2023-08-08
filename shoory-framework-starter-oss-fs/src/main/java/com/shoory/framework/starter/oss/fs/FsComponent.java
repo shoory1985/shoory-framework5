c84013517987b26b0c124554f07d274e2a51cc7f
shoory@qq.com <shoory@qq.com>
2023-08-09 07:15:47 +0800

    starter-oss-fs的upload方法扩展，支持自动创建文件夹并设置权限2

--- shoory-framework-starter-oss-fs/src/main/java/com/shoory/framework/starter/oss/fs/FsComponent.java
+++ shoory-framework-starter-oss-fs/src/main/java/com/shoory/framework/starter/oss/fs/FsComponent.java
@@ -1,6 +1,7 @@
 package com.shoory.framework.starter.oss.fs;
 
 import com.shoory.framework.starter.oss.OssComponent;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,18 +14,44 @@ public class FsComponent implements OssComponent {
 
 	public String upload(String prefix, String path, String mimeType, InputStream is) {
 		try {
-			File file = new File(basePath + prefix + path);
-			OutputStream os = new FileOutputStream(file);
+			//检查/创建文件夹
+			String realPath = this.basePath + prefix;
+			{
+				String[] pieces = path.split("/");	//切割
+				for (int i = 0; i < pieces.length; i++) {
+					if (StringUtils.isNotBlank(pieces[i])) {
+						realPath += pieces[i];
+						if (i < pieces.length - 1) {
+							//创建文件夹
+							File dir = new File(realPath);
+							if (!dir.exists()) {
+								dir.mkdir();
+								dir.setWritable(true);
+								dir.setReadable(true);
+								dir.setExecutable(true);
+							}
+							realPath += "/";
+						}
+					}
+				}
+			}
 
+			//写入文件
+			FileOutputStream os = new FileOutputStream(realPath);
 			byte[] b = new byte[1024];
 			while ((is.read(b)) != -1) {
 				os.write(b);// 写入数据
 			}
 			is.close();
-			os.close();// 保存数据
+			os.flush();
+			os.close();
+
+			File file = new File(realPath);
+			file.setWritable(true);
+			file.setReadable(true);
 
 			return path;
-		} catch (Throwable e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
 		}
